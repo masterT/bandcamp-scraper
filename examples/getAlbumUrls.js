d82355b8ac54b3a6ef5f1b9e3cc8e3a95b869c27
Simon Thiboutôt <simonthiboutot@hotmail.com>
2016-07-25 22:44:06 -0400

    Add output in examples

--- examples/getAlbumUrls.js
+++ examples/getAlbumUrls.js
@@ -8,3 +8,14 @@ bandcamp.getAlbumUrls(artistUrl, function(error, albumUrls) {
     console.log(albumUrls);
   }
 });
+
+/*
+[ 'http://musique.coeurdepirate.com/album/roses',
+  'http://musique.coeurdepirate.com/album/carry-on-2',
+  'http://musique.coeurdepirate.com/album/oublie-moi-carry-on',
+  'http://musique.coeurdepirate.com/album/child-of-light',
+  'http://musique.coeurdepirate.com/album/trauma',
+  'http://musique.coeurdepirate.com/album/blonde',
+  'http://musique.coeurdepirate.com/album/coeur-de-pirate',
+  'http://musique.coeurdepirate.com/album/comme-des-enfants-version-originale-et-remix-par-le-matos' ]
+*/
